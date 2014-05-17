e18efdca7569baceb08b5224b2114d9a9e362f95
Satoshi Yagi <satoshi.yagi@yahoo.com>
2014-05-16 23:16:51 -0700

    initial commit

---
+++ modules/bowling.py
@@ -0,0 +1,38 @@
+class Bowling(object):
+    def __init__(self):
+        self.rolls = list()
+    
+    def roll(self, pins):
+        self.rolls.append(pins)
+
+    def calculate_score(self):
+        counter = 0
+        score = 0
+        
+        for i in range(10):
+            first_roll = self.rolls[counter]
+            second_roll = self.rolls[counter+1]
+            
+            if self.is_strike(first_roll):
+                third_roll = self.rolls[counter+2]
+                score += first_roll + second_roll + third_roll
+                counter += 1
+            elif self.is_spare(first_roll, second_roll):
+                third_roll = self.rolls[counter+2]
+                score += first_roll + second_roll + third_roll
+                counter += 2
+            else:
+                score += first_roll + second_roll
+                counter += 2
+
+        return score
+    
+    def is_strike(self, first):
+        if first == 10:
+            return 1
+        return 0
+    
+    def is_spare(self, first, second):
+        if self.is_strike(first) or first + second != 10:
+            return 0
+        return 1
---
+++ tests/unit.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import unittest
+
+findbin = os.path.abspath(os.path.dirname(sys.argv[0]))
+sys.path.append(findbin + "/../modules")
+
+from bowling import Bowling
+
+class TestSequenceFunctions(unittest.TestCase):
+    def setUp(self):
+        self.game = Bowling()
+
+    def test_gutter_game(self):
+        g = self.game
+        self.roll_many(20, 0)
+        self.assertEqual(0, g.calculate_score(), 'gutter game 0 points')
+
+    def test_one_pin_game(self):
+        g = self.game
+        g.roll(1)
+        self.roll_many(19, 0)
+        self.assertEquals(1, g.calculate_score(), 'one pin game 1 points')
+
+    def test_one_spare_game(self):
+        g = self.game
+        g.roll(5)
+        g.roll(5)
+        g.roll(7)
+        self.roll_many(17, 0)
+        self.assertEquals(24, g.calculate_score(), 'one spare game 24 points')
+
+    def test_one_strike_game(self):
+        g = self.game
+        g.roll(10)
+        g.roll(2)
+        g.roll(3)
+        self.roll_many(17, 0)
+        self.assertEquals(20, g.calculate_score(), 'one strike game 20 points')
+        
+    def test_perfect_game(self):
+        g = self.game
+        self.roll_many(12, 10)
+        self.assertEquals(300, g.calculate_score(), 'perfect game 300 points')
+
+    def roll_many(self, rolls, pins):
+        g = self.game
+
+        for i in range(rolls):
+            g.roll(pins)
+
+if __name__ == '__main__':
+    unittest.main()
