942ce9cfb880be226c2b0e08c9e2c5d5ae720e5c
Satoshi Yagi <syagi@proofpoint.com>
2015-09-17 11:58:43 -0700

    test

--- tests/bowling_test.py
+++ tests/bowling_test.py
@@ -3,9 +3,5 @@
 import unittest
 from bowling.game import BowlingGame
 
-class TestBowling(unittest.TestCase):
-    def test_guttergame(self):
-        g = BowlingGame()
-
 if __name__ == '__main__':
     unittest.main()
