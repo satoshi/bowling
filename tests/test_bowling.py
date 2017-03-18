c78b4286278fa3a6e0ee1b21fb23cd10c8d6c4a6
satoshi <satoshi.yagi@yahoo.com>
2017-03-18 16:04:35 -0700

    changing tests to pytest

--- tests/test_bowling.py
+++ tests/test_bowling.py
@@ -1,47 +1,49 @@
 #!/usr/bin/python
 
-import unittest
 from bowling.game import BowlingGame
 
 
-class TestBowlingGame(unittest.TestCase):
+def setup():
+    return BowlingGame()
 
-    def setUp(self):
-        self.game = BowlingGame()
 
-    def test_guttergame(self):
-        g = self.game
-        self.roll_many(0, 20)
-        self.assertEquals(0, g.calculate_score(), 'gutter game score 0')
+def roll_many(g, pins, rolls):
+    for i in range(rolls):
+        g.roll(pins)
 
-    def test_one_pin_game(self):
-        g = self.game
-        g.roll(1)
-        self.roll_many(0, 19)
-        self.assertEquals(1, g.calculate_score(), 'one pin game score 1')
 
-    def test_one_spare(self):
-        g = self.game
-        g.roll(7)
-        g.roll(3)
-        g.roll(5)
-        self.roll_many(0, 17)
-        self.assertEquals(20, g.calculate_score(), 'one spare game score 20')
+def test_guttergame():
+    g = setup()
+    roll_many(g, 0, 20)
+    assert g.calculate_score() == 0, 'gutter game score 0'
 
-    def test_one_strike(self):
-        g = self.game
-        g.roll(10)
-        g.roll(2)
-        g.roll(4)
-        self.roll_many(0, 17)
-        self.assertEquals(22, g.calculate_score(), 'one strike game score 22')
 
-    def test_perfect_game(self):
-        g = self.game
-        self.roll_many(10, 12)
-        self.assertEquals(300, g.calculate_score(), 'perfect game score 300')
+def test_one_pin_game():
+    g = setup()
+    g.roll(1)
+    roll_many(g, 0, 19)
+    assert g.calculate_score() == 1, 'one pin game score 1'
 
-    def roll_many(self, pins, rolls):
-        g = self.game
-        for i in range(rolls):
-            g.roll(pins)
+
+def test_one_spare():
+    g = setup()
+    g.roll(7)
+    g.roll(3)
+    g.roll(5)
+    roll_many(g, 0, 17)
+    assert g.calculate_score() == 20, 'one spare game score 20'
+
+
+def test_one_strike():
+    g = setup()
+    g.roll(10)
+    g.roll(2)
+    g.roll(4)
+    roll_many(g, 0, 17)
+    assert g.calculate_score() == 22, 'one strike game score 22'
+
+
+def test_perfect_game():
+    g = setup()
+    roll_many(g, 10, 12)
+    assert g.calculate_score() == 300, 'perfect game score 300'
--- tests/test_frame.py
+++ tests/test_frame.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python
 
-import unittest
 from bowling.frame import BowlingFrame
 
 
-class TestBowlingFrame(unittest.TestCase):
+def test_normal_frame():
+    f = BowlingFrame([3, 5])
+    assert f.calculate_score() == 8, 'got 8 for the frame'
+    assert f.calculate_offset() == 2, 'got 2 for offset'
 
-    def test_normal_frame(self):
-        f = BowlingFrame([3, 5])
-        self.assertEquals(f.calculate_score(), 8, 'got 8 for the frame')
-        self.assertEquals(f.calculate_offset(), 2, 'got 2 for offset')
 
-    def test_spare_frame(self):
-        f = BowlingFrame([6, 4, 8])
-        self.assertEquals(f.calculate_score(), 18, 'got 18 for the frame')
-        self.assertEquals(f.calculate_offset(), 2, 'got 2 for offset')
+def test_spare_frame():
+    f = BowlingFrame([6, 4, 8])
+    assert f.calculate_score() == 18, 'got 18 for the frame'
+    assert f.calculate_offset() == 2, 'got 2 for offset'
 
-    def test_strike_frame(self):
-        f = BowlingFrame([10, 3, 6])
-        self.assertEquals(f.calculate_score(), 19, 'got 19 for the frame')
-        self.assertEquals(f.calculate_offset(), 1, 'got 1 for offset')
+
+def test_strike_frame():
+    f = BowlingFrame([10, 3, 6])
+    assert f.calculate_score() == 19, 'got 19 for the frame'
+    assert f.calculate_offset() == 1, 'got 1 for offset'
